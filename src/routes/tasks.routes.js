d3d413219369a7105381576a9ae17c6a1ca661c9
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-08-17 14:41:41 -0300

    1 file modified, 1 added

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,9 +1,20 @@
 import { Router } from 'express';
 import { authRequired } from '../middlewares/validateToken.js';
+import {
+  createTask,
+  deleteTask,
+  getTask,
+  getTasks,
+  updateTask,
+} from '../controllers/tasks.controller.js';
 
 const router = Router();
 
 router.use(authRequired);
-router.get('/tasks', (req, res) => res.send('tasks'));
+router.get('/tasks', getTasks);
+router.get('/tasks/:id', getTask);
+router.post('/tasks', createTask);
+router.delete('/tasks/:id', deleteTask);
+router.put('/tasks/:id', updateTask);
 
 export default router;
