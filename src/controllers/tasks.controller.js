081da62888ed0b6a88073097f6dbd8c963b10c9d
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-08-17 15:01:54 -0300

    2 files modified, 1 added

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,9 +1,38 @@
-export const getTasks = async (req, res) => {};
+import Task from '../models/task.model.js';
 
-export const createTask = async (req, res) => {};
+export const getTasks = async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+};
 
-export const getTask = async (req, res) => {};
+export const createTask = async (req, res) => {
+  const { title, description, date } = req.body;
+  const newTask = new Task({
+    title,
+    description,
+    date,
+  });
+  const savedTask = await newTask.save();
+  res.json(savedTask);
+};
 
-export const updateTask = async (req, res) => {};
+export const getTask = async (req, res) => {
+  const { id } = req.params;
+  const task = await Task.findById(id);
+  if (!task) res.status(404).json('Task not found!');
+  res.json(task);
+};
 
-export const deleteTask = async (req, res) => {};
+export const updateTask = async (req, res) => {
+  const { id } = req.params;
+  const task = await Task.findByIdAndDelete(id);
+  if (!task) res.status(404).json('Task not found!');
+  res.json(task);
+};
+
+export const deleteTask = async (req, res) => {
+  const { id } = req.params;
+  const task = await Task.findByIdAndUpdate(id, req.body, { new: true });
+  if (!task) res.status(404).json('Task not found!');
+  res.json(task);
+};
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,6 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose from 'mongoose';
 
-const userSchema = new Schema(
+const userSchema = new mongoose.Schema(
   {
     username: {
       type: String,
@@ -23,6 +23,6 @@ const userSchema = new Schema(
   }
 );
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 
 export default User;
